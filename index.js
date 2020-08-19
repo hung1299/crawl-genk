ed232a4ab29f82435e2dc9b1210c99316624208f
Hung <dotaallstar1999@gmail.com>
2020-08-19 23:26:55 +0700

    add Redirect

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use('/api/articles', articleRoute);
 
+app.get('/', (req, res) => {
+  res.redirect('/api/articles');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
